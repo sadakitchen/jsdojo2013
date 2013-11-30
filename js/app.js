9434a5d1d5ff86dbd93f4fa836df2df4693080f0
Sadahiro Takahashi <sadakitchen@Sadahiro-no-MacBook-Air.local>
2013-11-30 16:31:17 +0900

    * 回答するたびに問題数値がランダムで変更される

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var mainCtrl = function($scope){
   // 変数
-  var _ram = Math.random()*1000;
-  _ram = (_ram|0);  // Chromeで一番はやい小数点切り捨て
-  $scope.questionMsg = _ram;
+  // var _ram = Math.random()*1000;
+  // _ram = (_ram|0);  // Chromeで一番はやい小数点切り捨て
+  $scope.questionMsg = ramGene();
   $scope.resultMsg = "";
 
   // 関数
@@ -18,8 +18,20 @@ var mainCtrl = function($scope){
       $scope.resultMsg = "✕";
     }
     // $scope.resultMsg = "◯";
+
+    //    問題リセット
+    $scope.questionMsg = ramGene();
+
     $('.result').show();
     $('.result').fadeOut();
+  }
+
+  // private 
+  // @return 
+  function ramGene(){
+    var _num = Math.random()*1000;
+    _num = (_num|0);
+    return _num;
   }
 
   // private 
